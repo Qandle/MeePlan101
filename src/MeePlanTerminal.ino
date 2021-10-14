e3964c32f7102467583aa5e48987e79ac365fad7
nut1414 <nut.pannapat@gmail.com>
2021-10-14 22:10:29 +0700

    Initial base iot example

---
+++ src/MeePlanTerminal.ino
@@ -0,0 +1,126 @@
+//Modifed from HTTPS GET Example
+//to call a REST api and displaying data on WIO Terminal's screen.
+
+#include <ArduinoJson.h>
+#include <rpcWiFi.h>
+#include <HTTPClient.h>
+#include <WiFiClientSecure.h>
+#include "TFT_eSPI.h"
+
+TFT_eSPI tft;
+const char *ssid = "wifi here";
+const char *password = "password here";
+const char *test_root_ca =
+    "-----BEGIN CERTIFICATE-----\n"
+    "MIIFYDCCBEigAwIBAgIQQAF3ITfU6UK47naqPGQKtzANBgkqhkiG9w0BAQsFADA/\n"
+    "MSQwIgYDVQQKExtEaWdpdGFsIFNpZ25hdHVyZSBUcnVzdCBDby4xFzAVBgNVBAMT\n"
+    "DkRTVCBSb290IENBIFgzMB4XDTIxMDEyMDE5MTQwM1oXDTI0MDkzMDE4MTQwM1ow\n"
+    "TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh\n"
+    "cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwggIiMA0GCSqGSIb3DQEB\n"
+    "AQUAA4ICDwAwggIKAoICAQCt6CRz9BQ385ueK1coHIe+3LffOJCMbjzmV6B493XC\n"
+    "ov71am72AE8o295ohmxEk7axY/0UEmu/H9LqMZshftEzPLpI9d1537O4/xLxIZpL\n"
+    "wYqGcWlKZmZsj348cL+tKSIG8+TA5oCu4kuPt5l+lAOf00eXfJlII1PoOK5PCm+D\n"
+    "LtFJV4yAdLbaL9A4jXsDcCEbdfIwPPqPrt3aY6vrFk/CjhFLfs8L6P+1dy70sntK\n"
+    "4EwSJQxwjQMpoOFTJOwT2e4ZvxCzSow/iaNhUd6shweU9GNx7C7ib1uYgeGJXDR5\n"
+    "bHbvO5BieebbpJovJsXQEOEO3tkQjhb7t/eo98flAgeYjzYIlefiN5YNNnWe+w5y\n"
+    "sR2bvAP5SQXYgd0FtCrWQemsAXaVCg/Y39W9Eh81LygXbNKYwagJZHduRze6zqxZ\n"
+    "Xmidf3LWicUGQSk+WT7dJvUkyRGnWqNMQB9GoZm1pzpRboY7nn1ypxIFeFntPlF4\n"
+    "FQsDj43QLwWyPntKHEtzBRL8xurgUBN8Q5N0s8p0544fAQjQMNRbcTa0B7rBMDBc\n"
+    "SLeCO5imfWCKoqMpgsy6vYMEG6KDA0Gh1gXxG8K28Kh8hjtGqEgqiNx2mna/H2ql\n"
+    "PRmP6zjzZN7IKw0KKP/32+IVQtQi0Cdd4Xn+GOdwiK1O5tmLOsbdJ1Fu/7xk9TND\n"
+    "TwIDAQABo4IBRjCCAUIwDwYDVR0TAQH/BAUwAwEB/zAOBgNVHQ8BAf8EBAMCAQYw\n"
+    "SwYIKwYBBQUHAQEEPzA9MDsGCCsGAQUFBzAChi9odHRwOi8vYXBwcy5pZGVudHJ1\n"
+    "c3QuY29tL3Jvb3RzL2RzdHJvb3RjYXgzLnA3YzAfBgNVHSMEGDAWgBTEp7Gkeyxx\n"
+    "+tvhS5B1/8QVYIWJEDBUBgNVHSAETTBLMAgGBmeBDAECATA/BgsrBgEEAYLfEwEB\n"
+    "ATAwMC4GCCsGAQUFBwIBFiJodHRwOi8vY3BzLnJvb3QteDEubGV0c2VuY3J5cHQu\n"
+    "b3JnMDwGA1UdHwQ1MDMwMaAvoC2GK2h0dHA6Ly9jcmwuaWRlbnRydXN0LmNvbS9E\n"
+    "U1RST09UQ0FYM0NSTC5jcmwwHQYDVR0OBBYEFHm0WeZ7tuXkAXOACIjIGlj26Ztu\n"
+    "MA0GCSqGSIb3DQEBCwUAA4IBAQAKcwBslm7/DlLQrt2M51oGrS+o44+/yQoDFVDC\n"
+    "5WxCu2+b9LRPwkSICHXM6webFGJueN7sJ7o5XPWioW5WlHAQU7G75K/QosMrAdSW\n"
+    "9MUgNTP52GE24HGNtLi1qoJFlcDyqSMo59ahy2cI2qBDLKobkx/J3vWraV0T9VuG\n"
+    "WCLKTVXkcGdtwlfFRjlBz4pYg1htmf5X6DYO8A4jqv2Il9DjXA6USbW1FzXSLr9O\n"
+    "he8Y4IWS6wY7bCkjCWDcRQJMEhg76fsO3txE+FiYruq9RUWhiF1myv4Q6W+CyBFC\n"
+    "Dfvp7OOGAN6dEOM4+qR9sdjoSYKEBpsr6GtPAQw4dy753ec5\n"
+    "-----END CERTIFICATE-----\n";
+
+
+
+
+WiFiClientSecure client;
+DynamicJsonDocument doc(4096);
+
+void setup()
+{
+
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  tft.begin();
+  tft.setRotation(3);
+
+  tft.fillScreen(TFT_BLACK); // fills entire the screen with colour red
+  tft.setTextSize(1);
+
+  tft.setTextWrap(true);
+  tft.drawString("Connecting...", 0, 0);
+  while (WiFi.status() != WL_CONNECTED)
+  { //Check for the connection
+    delay(500);
+    Serial.println("Connecting..");
+  }
+  Serial.print("Connected to the WiFi network with IP: ");
+  Serial.println(WiFi.localIP());
+  client.setCACert(test_root_ca);
+}
+
+void loop()
+{
+  if (&client)
+  {
+    {
+      // Add a scoping block for HTTPClient https to make sure it is destroyed before WiFiClientSecure *client is
+      HTTPClient https;
+      Serial.print("[HTTPS] begin...\n");
+      //google ip as an example lol;
+      if (https.begin(client, "https://api.techniknews.net/ipgeo/8.8.8.8"))
+      { // HTTPS
+        Serial.print("[HTTPS] GET...\n");
+        // start connection and send HTTP header
+        int httpCode = https.GET();
+        // httpCode will be negative on error
+        if (httpCode > 0)
+        {
+          // HTTP header has been send and Server response header has been handled
+          Serial.printf("[HTTPS] GET... code: %d\n", httpCode);
+          // file found at server
+
+          String payload = https.getString();
+          deserializeJson(doc, payload);
+          JsonObject arr = doc.as<JsonObject>();
+          String ip = arr["ip"];
+          String text = arr["isp"];
+          tft.fillScreen(TFT_BLACK);
+          tft.drawString(text + ip, 0, 0);
+          Serial.println(text);
+          Serial.println(payload);
+        }
+        else
+        {
+          Serial.printf("[HTTPS] GET... failed, error: %s, code: %d\n", https.errorToString(httpCode).c_str(), httpCode);
+        }
+        https.end();
+      }
+      else
+      {
+        Serial.printf("[HTTPS] Unable to connect\n");
+      }
+      // End extra scoping block
+    }
+  }
+  else
+  {
+    Serial.println("Unable to create client");
+  }
+  Serial.println();
+  Serial.println("Waiting 10s before the next round...");
+  delay(5000);
+}
